c66d25e0e9d37918272f2d3119e869d234c340d1
Estuardo Díaz <estuardo@pixela.com.gt>
2020-06-29 00:44:04 -0600

    Adds all functions for initiatives

--- functions/initiativeName.js
+++ functions/initiativeName.js
@@ -4,5 +4,5 @@ exports.default = (snapshot, context) => {
   const number = Math.floor(Math.random() * 999) + 1
   const letter = letter.charAt(Math.floor(Math.random() * letters.length - 1))
 
-  original.ref.update({ name: letter + ' ' + number})
+  return snapshot.ref.update({ name: letter + ' ' + number})
 }
